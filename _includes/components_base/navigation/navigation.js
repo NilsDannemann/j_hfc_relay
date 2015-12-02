5d5acf7f77276f6a35f8f4dee15ecf5057b40632
Nils Dannemann <ndannemann@gmail.com>
2015-12-02 16:51:02 +0100

    [add] first header implementation + box-shadow mixin

--- _includes/components_base/navigation/navigation.js
+++ _includes/components_base/navigation/navigation.js
@@ -2,18 +2,18 @@ var ww = window.innerWidth;
 
 $(document).ready(function() {
 	
-	// Add nav__link--parent class
-	$(".nav__inner li a").each(function() {
+	// Add navigation__link--parent class
+	$(".navigation__inner li a").each(function() {
 		if ($(this).next().length > 0) {
-			$(this).addClass("nav__link--parent");
+			$(this).addClass("navigation__link--parent");
 		};
 	});
 	
-	// Add nav__more class
-	$('<div class="nav__more"></div>').insertBefore($('.nav__link--parent'));
+	// Add navigation__more class
+	$('<div class="navigation__more"></div>').insertBefore($('.navigation__link--parent'));
 	
 	// Toggle Navigation
-	$(".nav__icon").click(function(e) {
+	$(".navigation__icon").click(function(e) {
 		e.preventDefault();
 		$(this).toggleClass("is-active");
 	});
@@ -28,16 +28,16 @@ $(window).bind('resize orientationchange', function() {
 
 var adjustMenu = function() {
 	if (ww < 800) {
-		$(".nav__inner li").unbind('mouseenter mouseleave');
-		$(".nav__inner li a.nav__link--parent").unbind('click');
-		$(".nav__inner li .nav__more").unbind('click').bind('click', function() {
+		$(".navigation__inner li").unbind('mouseenter mouseleave');
+		$(".navigation__inner li a.navigation__link--parent").unbind('click');
+		$(".navigation__inner li .navigation__more").unbind('click').bind('click', function() {
 			$(this).parent("li").toggleClass("is-hovered");
 		});
 	} 
 	else if (ww > 800) {
-		$(".nav__inner li").removeClass("is-hovered");
-		$(".nav__inner li a").unbind('click');
-		$(".nav__inner li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+		$(".navigation__inner li").removeClass("is-hovered");
+		$(".navigation__inner li a").unbind('click');
+		$(".navigation__inner li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
 			// must be attached to li so that mouseleave is not triggered when is-hovered over submenu
 			$(this).toggleClass("is-hovered");
 		});
