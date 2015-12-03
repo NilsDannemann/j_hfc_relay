1dd1a8d08257e37b6769d60ef0be60a4dd030fb8
Nils Dannemann <ndannemann@gmail.com>
2015-12-03 16:47:15 +0100

    [update] renamed sass partials

--- _includes/styleguide/component_list/component_list.js
+++ _includes/styleguide/component_list/component_list.js
@@ -1,6 +1,6 @@
 <script>
 	(function (document, undefined) {
-		document.getElementById('component_nav').onchange = function() {
+		document.getElementById('component_list').onchange = function() {
 			var val = this.value;
 			if (val !== "") {
 				window.location = val;
