3f3d90ce0cb148fea4c6922706d7ad10440891d8
Nils Dannemann <ndannemann@gmail.com>
2016-01-20 09:04:18 +0100

    [update] integrated gulp image optimization

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	jpegtran = require('imagemin-jpegtran'),
+	gifsicle = require('imagemin-gifsicle'),
+	optipng = require('imagemin-optipng');
 
-gulp.task('default', function() {
+var outputfolder = '.deploy';
 
+// MAIN - Use "gulp" to run all tasks in order
+gulp.task('default', ['images']);
+
+// IMAGE - Compress images put in outputfolder
+gulp.task('images', function () {
+    return gulp.src('assets/images/**')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant(), jpegtran(), optipng(), gifsicle()]
+        }))
+        .pipe(gulp.dest(outputfolder + '/assets/images'));
 });
