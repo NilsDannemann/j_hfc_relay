cd9f5ff6068ded7d2e71971b0f0393bc35efa701
Nils Dannemann <ndannemann@gmail.com>
2016-01-20 09:53:42 +0100

    [update] gulp tasks: build, images, html

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,22 +1,43 @@
 var gulp = require('gulp'),
+	// Jekyll build
+	shell = require('gulp-shell'),
+	// HTML
+	htmlmin = require('gulp-htmlmin');
+	// Images
 	imagemin = require('gulp-imagemin'),
 	pngquant = require('imagemin-pngquant'),
 	jpegtran = require('imagemin-jpegtran'),
 	gifsicle = require('imagemin-gifsicle'),
-	optipng = require('imagemin-optipng');
+	optipng = require('imagemin-optipng'),
+	// Folder
+	outputfolder = '.deploy';
 
-var outputfolder = '.deploy';
 
-// MAIN - Use "gulp" to run all tasks in order
-gulp.task('default', ['images']);
 
-// IMAGE - Compress images put in outputfolder
-gulp.task('images', function () {
-    return gulp.src('assets/images/**')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant(), jpegtran(), optipng(), gifsicle()]
-        }))
-        .pipe(gulp.dest(outputfolder + '/assets/images'));
+// gulp - run all tasks in order
+gulp.task('default', ['html', 'images']);
+
+// gulp build - Triggers Jekylls build command
+gulp.task('build', shell.task([
+	'jekyll build'
+]));
+
+// gulp html - Compress html and put in outputfolder
+gulp.task('html', ['build'], function() {
+	return gulp.src(outputfolder + '/**/*.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
+		.pipe(gulp.dest(outputfolder));
+});
+
+// gulp images - Compress images and put in outputfolder
+gulp.task('images', ['build'], function () {
+	return gulp.src('assets/images/**')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant(), jpegtran(), optipng(), gifsicle()]
+		}))
+		.pipe(gulp.dest(outputfolder + '/assets/images'));
 });
