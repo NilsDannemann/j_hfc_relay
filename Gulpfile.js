7f73d44675ebded1b0825bb7dc9a0a5ec325037b
Nils Dannemann <ndannemann@gmail.com>
2016-01-26 10:26:35 +0100

    [add] gulp notifications

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,9 @@ GULP VARIABLES
 var gulp = require('gulp'),
 	shell = require('gulp-shell'),
 	runSequence = require('run-sequence'),
+	replace = require('gulp-replace'),	
+	fs = require('fs'),
+	notify = require('gulp-notify'),		
 	// For: gulp html
 	htmlmin = require('gulp-htmlmin'),				//works - (minifies html)
 	// For: gulp images
@@ -13,7 +16,6 @@ var gulp = require('gulp'),
 	jpegtran = require('imagemin-jpegtran'),		//works - (minify jpg)
 	gifsicle = require('imagemin-gifsicle'),		//works - (minify gifsicle)
 	// For: gulp css
-	replace = require('gulp-replace'),				//works - (remove jekyll frontmatter before processing)
 	sass = require('gulp-sass'),					//works - (process sass)
 	importCss = require('gulp-import-css'),			//works - (imports from remote sources like a CDN)
 	autoprefixer = require('gulp-autoprefixer'), 	//works	- (autoprefixes)
@@ -30,14 +32,14 @@ var gulp = require('gulp'),
 
 
 /*===========================
-GULP BUILD || run all tasks in order
+GULP OPTIMIZE || run all tasks in order
 ===========================*/
-gulp.task('build', function(callback) {
+gulp.task('optimize', function(callback) {
 	runSequence(
 		'jekyll_build',
-		'html', 
-		'css',
-		'images', 
+		'optimize html', 
+		'optimize css',
+		'optimize images', 
 		'publish',
 		callback);
 });
@@ -56,15 +58,22 @@ gulp.task('jekyll_serve', shell.task(['jekyll serve']));
 /*===========================
 GULP HTML || compress html + inline css + inline scripts & put in outputfolder
 ===========================*/
-gulp.task('html', function() {
+gulp.task('optimize html', function() {
 	return gulp.src(outputfolder + '/**/*.html')
+		.pipe(notify({message: '[HTML] ------------------------', onLast: true}))
+		.pipe(notify({message: '[HTML] - removing comments...', onLast: true}))
+		.pipe(notify({message: '[HTML] - minifying inline scripts...', onLast: true}))
+		.pipe(notify({message: '[HTML] - minifying inline styles...', onLast: true}))
+		.pipe(notify({message: '[HTML] - removing whitespace...', onLast: true}))
+		.pipe(notify({message: '[HTML] - saving html files...', onLast: true}))
 		.pipe(htmlmin({
 			removeComments: true,			//works - removes comments
 			minifyJS: true,					//works - minifies inline scripts
 			minifyCSS: true,				//works - minifies inline styles
 			collapseWhitespace: true		//works - collapses whitespace
 		}))
-		.pipe(gulp.dest(outputfolder));
+		.pipe(gulp.dest(outputfolder))
+		.pipe(notify({message: '[HTML] ------------------------', onLast: true}));
 });
 
 
@@ -73,14 +82,20 @@ gulp.task('html', function() {
 /*===========================
 GULP IMAGES || compress images & put in outputfolder
 ===========================*/
-gulp.task('images', function () {
+gulp.task('optimize images', function () {
 	return gulp.src('assets/images/**')
+		.pipe(notify({message: '[IMAGES] ------------------------', onLast: true}))
+		.pipe(notify({message: '[IMAGES] - optimizing jpgs...', onLast: true}))
+		.pipe(notify({message: '[IMAGES] - optimizing pngs...', onLast: true}))
+		.pipe(notify({message: '[IMAGES] - optimizing gifs...', onLast: true}))
+		.pipe(notify({message: '[IMAGES] - saving optimized images...', onLast: true}))
 		.pipe(imagemin({
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
 			use: [pngquant(), jpegtran(), optipng(), gifsicle()]
 		}))
-		.pipe(gulp.dest(outputfolder + '/assets/images'));
+		.pipe(gulp.dest(outputfolder + '/assets/images'))
+		.pipe(notify({message: '[IMAGES] ------------------------', onLast: true}));
 });
 
 
@@ -91,26 +106,34 @@ GULP CSS || compress css & put in outputfolder
 ===========================*/
 
 // Step 1 - Wait for jekyll_build, then include style.min.css
-gulp.task('css_add_mincss_to_head', function() {
+gulp.task('create style.min.css', function() {
 	return gulp.src(outputfolder + '/**/*.html')
 		.pipe(replace('style.css', 'style.min.css'))
 		.pipe(gulp.dest(outputfolder));
 });
 // Step 2 - Minify everything, then create style.min.css
-gulp.task('css', ['css_add_mincss_to_head'], function() {
+gulp.task('optimize css', ['create style.min.css'], function() {
 	return gulp.src('assets/css/style.scss')
+		.pipe(notify({message: '[CSS] ------------------------', onLast: true}))
+		.pipe(notify({message: '[CSS] - preparing scss files...', onLast: true}))
 		.pipe(replace('---', ''))
 		.pipe(replace('@import \'', '@import \'_includes/'))
 		.pipe(sass())
+		.pipe(notify({message: '[CSS] - importing remote styles...', onLast: true}))
 		.pipe(importCss())
+		.pipe(notify({message: '[CSS] - autoprefixing...', onLast: true}))
 		.pipe(autoprefixer())
+		.pipe(notify({message: '[CSS] - removing unused styles...', onLast: true}))
 		.pipe(uncss({
 			html: glob.sync(".deploy/**/*.html"),
 			ignore: ['is-*', 'has-*', '*--active']
 		}))
+		.pipe(notify({message: '[CSS] - minifying...'}))
 		.pipe(cssnano())
+		.pipe(notify({message: '[CSS] - creating styles.min.css...', onLast: true}))
 		.pipe(rename('style.min.css'))
-		.pipe(gulp.dest(outputfolder + '/assets/css'));
+		.pipe(gulp.dest(outputfolder + '/assets/css'))
+		.pipe(notify({message: '[CSS] ------------------------', onLast: true}));
 });
 
 
@@ -145,4 +168,19 @@ GULP PUBLISH || PUBLISH ON GITHUB
 ===========================*/
 gulp.task('publish', function() {
 	// in progress
+});
+
+
+
+/*===========================
+GULP CSSINLINE || INLINE CSS IN HEAD (for google page speed)
+===========================*/
+gulp.task('cssinline', ['optimize css'], function() {
+	return gulp.src(outputfolder + '/**/*.html')
+		.pipe(replace('<link rel=\"stylesheet\" href=\"/assets/css/style.min.css\">', function(s) {
+			var style = fs.readFileSync(outputfolder + '/assets/css/style.min.css', 'utf8');
+			return '<style>\n' + style + '\n</style>';
+		}))
+		.pipe(gulp.dest(outputfolder))
+		.pipe(notify({message: '[SUCESS] CSS Inlined in head', onLast: true}));
 });
